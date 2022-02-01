92f1912214aea0b3ed40f9d09cd5a3c16fe2df88
Omar Bitar <o.bitar97@gmail.com>
2022-02-01 14:07:59 -0700

    created fragment transaction for stopwatch

--- Workout_ch9/app/src/main/java/com/hfad/workout_ch9/TempActivity.java
+++ Workout_ch9/app/src/main/java/com/hfad/workout_ch9/TempActivity.java
@@ -1,6 +1,8 @@
 package com.hfad.workout_ch9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 public class TempActivity extends AppCompatActivity {
@@ -9,5 +11,14 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        // code for fragment transactiono
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container,stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
     }
 }
